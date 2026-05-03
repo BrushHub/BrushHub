ca939e20741f45300694a85eeeef9ab41c0fb4a4
BrushHub <matheuscraft137545@gmail.com>
2026-05-03 14:44:10 -0300

    Create GameList.lua

---
+++ Scripts/GameList.lua
@@ -0,0 +1,8 @@
+local Games = {
+  -- Omini X
+  [131073412590872] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Omini.lua", -- Game
+  [101026405220822] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Omini.lua", -- Raid
+  -- ===================================================================================== --
+  
+}
+return Games
