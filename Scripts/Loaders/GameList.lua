097ce1894926089a185255e7b6bc6ba34bb86d17
BrushHub <matheuscraft137545@gmail.com>
2026-05-16 21:58:04 -0300

    Update GameList.lua

--- Scripts/Loaders/GameList.lua
+++ Scripts/Loaders/GameList.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [131073412590872] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Omini X (Game)
-  [101026405220822] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Muscle%20Legends.lua", -- Omini X (Raid)
+  [131073412590872] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Omini.lua", -- Omini X (Game)
+  [101026405220822] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Omini.lua", -- Omini X (Raid)
   [0] = "0", -- Exemple
 return Games
