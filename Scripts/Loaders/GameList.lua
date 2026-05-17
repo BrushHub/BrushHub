1659ea69204f0585a5ed82cb46c7d5c574d389c5
BrushHub <matheuscraft137545@gmail.com>
2026-05-16 21:55:17 -0300

    Add GameList.lua with game URLs

---
+++ Scripts/Loaders/GameList.lua
@@ -0,0 +1,5 @@
+local Games = {
+  [131073412590872] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Omini X (Game)
+  [101026405220822] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Muscle%20Legends.lua", -- Omini X (Raid)
+  [0] = "0", -- Exemple
+return Games
