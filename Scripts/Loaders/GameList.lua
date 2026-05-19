742b922d1593154809511ce796abdb8d473423e8
BrushHub <matheuscraft137545@gmail.com>
2026-05-19 16:49:04 -0300

    Update GameList.lua

--- Scripts/Loaders/GameList.lua
+++ Scripts/Loaders/GameList.lua
@@ -1,7 +1,7 @@
 local Games = {
 	[131073412590872] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Omini.lua",
 	[101026405220822] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Omini.lua",
-	[537413528] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Build.lua"
+	[537413528] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Build.lua",
 }
 
 return Games
