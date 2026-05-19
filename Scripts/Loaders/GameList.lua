fba19b572c38a93b24ef67c7d3efc01ec882088f
BrushHub <matheuscraft137545@gmail.com>
2026-05-19 15:41:42 -0300

    Update GameList.lua

--- Scripts/Loaders/GameList.lua
+++ Scripts/Loaders/GameList.lua
@@ -1,5 +1,6 @@
 local Games = {
-  [131073412590872] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Omini.lua", -- Omini X (Game)
-  [101026405220822] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Omini.lua", -- Omini X (Raid)
-  [0] = "0", -- Exemple
+	[131073412590872] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Omini.lua",
+	[101026405220822] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Omini.lua",
+}
+
 return Games
