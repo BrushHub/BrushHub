ecb2f03b672da33b8d50f8550259c240163e9c0b
BrushHub <matheuscraft137545@gmail.com>
2026-05-22 11:48:38 -0300

    Update GameList.lua

--- Scripts/Loaders/GameList.lua
+++ Scripts/Loaders/GameList.lua
@@ -1,7 +1,7 @@
 local Games = {
 	[131073412590872] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Omini.lua",-- Omini X
 	[101026405220822] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Omini.lua", -- Omini X
-		[537413528] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Build.lua", -- Build a boat
+	[537413528] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Build.lua", -- Build a boat
 	[142823291] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Killer.lua", -- Murder Mystry 2 (BETA)
 }
 
