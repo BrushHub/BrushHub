50b0142116da98b43371a49f420ef22ca3b3d529
BrushHub <matheuscraft137545@gmail.com>
2026-05-22 16:02:36 -0300

    Update GameList.lua

--- Scripts/Loaders/GameList.lua
+++ Scripts/Loaders/GameList.lua
@@ -3,8 +3,8 @@ local Games = {
 	[101026405220822] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Omini.lua", -- Omini X
 	[537413528] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Build.lua", -- Build a boat
 	[142823291] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Killer.lua", -- Murder Mystry 2 (BETA)
-    [3101667897] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Speed.lua",
-	[79546208627805] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Night.lua",
+    [3101667897] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Speed.lua", -- Speed Legends
+	[79546208627805] = "https://raw.githubusercontent.com/BrushHub/BrushHub/refs/heads/main/Scripts/Brush%20Night.lua", -- 99 Nights
 
 }
 
